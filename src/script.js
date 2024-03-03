3204e9e7951f6b4021ce1f86b3691220d1f78a0d
Matías <crosserain@gmail.com>
2024-03-03 17:56:56 -0300

    reset board, get board and player factory

--- src/script.js
+++ src/script.js
@@ -1,17 +1,40 @@
-const gameBoard = (function () {
+const Gameboard = (function () {
     const board = [];
-
     //creates board
     for (let i = 0; i < 3; i++) {
         board.push([]);
         for (let j = 0; j < 3; j++) {
-            board[i].push("X");
+            board[i].push("");
         }
     }
 
-    return { board };
+    const getBoard = () => board;
+
+    //Marks a cell in a specified position
+    const markCell = (mark, col, row) => {
+        if (board[row][col]) {
+            throw new Error("Position already occupied");
+        }
+        board[row][col] = mark;
+    };
+
+    //Cleans the gameboard
+    const reset = () => {
+        board.forEach((row, i) => {
+            row.forEach((col, j) => (board[i][j] = ""));
+        });
+    };
+
+    return { getBoard, markCell, reset };
 })();
 
-//TODO Players object - PlayRound object
 
-console.log(gameBoard.board);
+
+
+//Create player factory function
+function Player(name, marker) {
+    const getName = () => name;
+    const getMarker = () => marker;
+
+    return { getName, getMarker };
+}
