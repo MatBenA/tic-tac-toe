5e6e08813eb27d4c0994bb8a5671ed314f64ad8b
Matías <crosserain@gmail.com>
2024-03-04 15:24:24 -0300

    play turn function added, winning state is now private

--- src/script.js
+++ src/script.js
@@ -29,12 +29,23 @@ const Gameboard = (function () {
     return { getBoard, markCell, reset };
 })();
 
-const Game = (function (board) {
-    /*  TODO recognize not only Xs but Os too */
+const Game = (function ({ getBoard, markCell, reset }) {
+    //TODO should put mark on gameboard and check winning state
+    const playTurn = (mark, row, col) => {
+        markCell(mark, row, col);
+        return winningState(mark);
+    };
+
+    //checks if given mark has won or the game is in draw state
     const winningState = (mark) => {
+        //this method could be refactored to check for dinamic number of rows and columns
+        const board = getBoard();
         //win state in rows
         for (let i = 0; i < board.length; i++) {
             if (
+                //for future scaling this could be inside 
+                //of a for loop that increments the columns or rows
+                //instead of having to do it manually
                 board[i][0] === mark &&
                 board[i][1] === mark &&
                 board[i][2] === mark
@@ -55,12 +66,20 @@ const Game = (function (board) {
         }
 
         //Win state diagonal
-        if (board[0][0] === mark && board[1][1] === mark && board[2][2] === mark) {
+        if (
+            board[0][0] === mark &&
+            board[1][1] === mark &&
+            board[2][2] === mark
+        ) {
             return `${mark} wins`;
         }
 
         //Win state reverse-diagonal
-        if (board[0][2] === mark && board[1][1] === mark && board[2][0] === mark) {
+        if (
+            board[0][2] === mark &&
+            board[1][1] === mark &&
+            board[2][0] === mark
+        ) {
             return `${mark} wins`;
         }
 
@@ -75,8 +94,9 @@ const Game = (function (board) {
         }
         return isFull && "draw";
     };
-    return { winningState };
-})(Gameboard.getBoard());
+
+    return { playTurn };
+})(Gameboard);
 
 //Create player factory function
 function Player(name, marker) {
