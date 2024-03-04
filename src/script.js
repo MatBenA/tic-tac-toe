7cebe8dcbecf2ecddf0a59c24c31d6a5c4b38304
Matías <crosserain@gmail.com>
2024-03-03 23:17:49 -0300

    win state in diagonals

--- src/script.js
+++ src/script.js
@@ -60,6 +60,16 @@ const Game = (function (board) {
                 return "X wins";
             }
         }
+
+        //Win state diagonal
+        if(board[0][0] && board[1][1] && board[2][2]){
+            return "X wins";
+        }
+
+        //Win state reverse-diagonal
+        if(board[0][2] && board[1][1] && board[2][0]){
+            return "X wins";
+        }
     };
     return { winningState };
 })(Gameboard.getBoard());
